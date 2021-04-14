648a5bc6606aaf4e3e449b76570e3ed539c10bb6
joeyyeoj <62258312+joeyyeoj@users.noreply.github.com>
2021-04-14 21:35:34 +0200

    commit

--- Programma/detect_mask_video.py
+++ Programma/detect_mask_video.py
@@ -18,15 +18,18 @@ import winsound
 img_goed = cv2.imread('groen.png')
 #lees rode plaatje in
 img_fout = cv2.imread('rood.png')
+#lees het grijze plaatje in
+img_geen = cv2.imread('grijs.png')
 masker = False
 last_mask = False
-
-l_img = img_goed
+l_img = img_geen
 sound = None
 
+face_detected = False
 
 
 def detect_and_predict_mask(frame, faceNet, maskNet):
+
 	# grab the dimensions of the frame and then construct a blob
 	# from it
 	(h, w) = frame.shape[:2]
@@ -39,6 +42,7 @@ def detect_and_predict_mask(frame, faceNet, maskNet):
 
 	# initialize our list of faces, their corresponding locations,
 	# and the list of predictions from our face mask network
+
 	faces = []
 	locs = []
 	preds = []
@@ -52,37 +56,29 @@ def detect_and_predict_mask(frame, faceNet, maskNet):
 		# filter out weak detections by ensuring the confidence is
 
 		if confidence > args["confidence"]:
-
 			box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
 			(startX, startY, endX, endY) = box.astype("int")
-
 		#framw
 			(startX, startY) = (max(0, startX), max(0, startY))
 			(endX, endY) = (min(w - 1, endX), min(h - 1, endY))
-
-
 			face = frame[startY:endY, startX:endX]
 			face = cv2.cvtColor(face, cv2.COLOR_BGR2RGB)
 			face = cv2.resize(face, (224, 224))
 			face = img_to_array(face)
 			face = preprocess_input(face)
 			face = np.expand_dims(face, axis=0)
-
 			# add the face and bounding boxes to their respective
 			# lists
 			faces.append(face)
 			locs.append((startX, startY, endX, endY))
-
 	# only make a predictions if at least one face was detected
 	if len(faces) > 0:
 		# for faster inference we'll make batch predictions on *all*
 		# faces at the same time rather than one-by-one predictions
 		# in the above `for` loop
 		preds = maskNet.predict(faces)
+	return locs, preds
 
-	# return a 2-tuple of the face locations and their corresponding
-	# locations
-	return (locs, preds)
 
 # construct the argument parser and parse the arguments
 ap = argparse.ArgumentParser()
@@ -136,20 +132,18 @@ while True:
 		# determine the class label and color we'll use to draw
 		# the bounding box and text
 		last_mask = masker
-
 		if mask > withoutMask:
 			color = (0, 255, 0)
 			l_img = img_goed
 			masker = True
 			#hieronder een string met de locatie van het bestand dat afgespeeld moet worden als iemand een masker op heeft
-			sound = 'C:/Users/joeyp/Desktop/IKPHBV_Project/Programma/IKPHBV_Draag_Je_Masker_Detectie_Tycho_van_Veen-Joey_Peschier/applause.wav'
-		else:
+			sound = 'C:/Users/joeyp/Desktop/IKPHBV_Project/IKPHBV_Draag_Je_Masker_Detectie_Tycho_van_Veen-Joey_Peschier/Programma/applause.wav'
+		elif withoutMask > mask:
 			l_img = img_fout
 			color = (0, 0, 255)
 			masker = False
 			# hieronder een string met de locatie van het bestand dat afgespeeld moet worden als iemand geen masker op heeft
-			sound = 'C:/Users/joeyp/Desktop/IKPHBV_Project/Programma/IKPHBV_Draag_Je_Masker_Detectie_Tycho_van_Veen-Joey_Peschier/beep.wav'
-
+			sound = 'C:/Users/joeyp/Desktop/IKPHBV_Project/IKPHBV_Draag_Je_Masker_Detectie_Tycho_van_Veen-Joey_Peschier/Programma/beep.wav'
 		#als de laatste keer dat er werd gecheckt de 'masker-status' anders is dan nu, speel geluid af.
 		if last_mask != masker:
 			winsound.PlaySound(sound,  winsound.SND_ASYNC)
